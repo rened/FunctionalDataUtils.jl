46e2bcf4e1e1fa72cf414f48893baa3fd9916e1f
Rene Donner <code@donner.at>
2015-04-16 14:48:03 +0200

    removed DictFiles dependency

--- src/FunctionalDataUtils.jl
+++ src/FunctionalDataUtils.jl
@@ -1,14 +1,14 @@
 module FunctionalDataUtils
 using Reexport
 @reexport using FunctionalData
-using Compat, SHA, FactCheck
+using Compat, SHA
+import FactCheck
 
 include("computing.jl")
 include("numerical.jl")
 include("computervision.jl")
 include("fmIO.jl")
 include("graphics.jl")
-include("io.jl")
 include("machinelearning.jl")
 # include("matlab.jl")
 include("numerical.jl")
