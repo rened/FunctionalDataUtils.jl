b02934e7185853846522b61cd55c43fd1780103d
Rene Donner <code@donner.at>
2015-08-07 10:12:24 +0200

    precompile

--- src/FunctionalDataUtils.jl
+++ src/FunctionalDataUtils.jl
@@ -1,6 +1,9 @@
+VERSION >= v"0.4.0-dev+6521" && __precompile__()
+
 module FunctionalDataUtils
 using Reexport
 @reexport using FunctionalData
+drop = FunctionalData.drop
 using Compat, SHA
 import FactCheck
 
