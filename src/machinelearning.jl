b44d09bd3aba78258cca5c7e00d734b89c70af9e
Rene Donner <code@donner.at>
2016-06-03 21:19:14 +0200

    nruns in crossval

--- src/machinelearning.jl
+++ src/machinelearning.jl
@@ -2,30 +2,28 @@ export crossval
 
 crossval(a::Tuple, args...; kargs...) = loocv(a..., args...; kargs...)
 
-function crossval(data, labels, train; predict = predict, n = 4, predictdata = [], shuffle = true,
-    nruns = n)
+function crossval(data, labels, train; predict = predict, n = 4, predictdata = [], shuffle = true)
     if !isempty(predictdata) && len(predictdata) != len(data)
         error("len(predictdata)==$(len(predictdata)) is not equal len(data)==$(len(data))")
     end
 
     ind = shuffle ? randperm(1:len(data)) : collect(1:len(data))
+    invind = invperm(ind)
 
     D = @p part data ind | partition n
     L = @p part labels ind | partition n
-    invind = @p invperm ind | partition n
 
     predictdata = isempty(predictdata) ? D : @p part predictdata ind | partition n
 
-    assert(nruns <= n)
-    r = cell(nruns)
-    for i in 1:nruns
+    r = cell(n)
+    for i in 1:n
         d1, _ = cut(D,i)
         _, d2 = cut(predictdata,i)
         l1, l2 = cut(L,i)
         m = train(flatten(d1),flatten(l1))
-        r[i] = @p predict m flatten(d2) | part invind[i]
+        r[i] = predict(m,flatten(d2))
     end
-    @p flatten r
+    @p flatten r | part invind
 end
 
 
